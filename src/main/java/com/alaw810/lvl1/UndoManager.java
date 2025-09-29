f879287298b2f416219d4fd19ce31da2dd9aec46
Adrià Lorente <adria810@gmail.com>
2025-09-29 13:20:12 +0200

    feat(lvl1): implement core singleton pattern

--- src/main/java/com/alaw810/lvl1/UndoManager.java
+++ src/main/java/com/alaw810/lvl1/UndoManager.java
@@ -1,4 +1,14 @@
 package com.alaw810.lvl1;
 
 public class UndoManager {
+    private static UndoManager instance;
+
+    private UndoManager() {}
+
+    public static UndoManager getInstance() {
+        if (instance == null) {
+            instance = new UndoManager();
+        }
+        return instance;
+    }
 }
--- src/test/java/com/alaw810/lvl1/UndoManagerTest.java
+++ src/test/java/com/alaw810/lvl1/UndoManagerTest.java
@@ -1,5 +1,16 @@
 package com.alaw810.lvl1;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UndoManagerTest {
+
+    @Test
+    void testSingletonInstanceUniqueness() {
+        UndoManager manager1 = UndoManager.getInstance();
+        UndoManager manager2 = UndoManager.getInstance();
+
+        assertSame(manager1, manager2, "Two calls must return the same instance.");
+    }
 
 }
